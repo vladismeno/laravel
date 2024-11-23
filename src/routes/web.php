d4f8778f0e6340a5253e21e36f8a74d09ea53bab
vladismeno <vladismeno@gmail.com>
2024-11-22 21:30:31 -0800

    Lesson 4

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $res = 1 + 3;
+    $name = 'svs';
+    return view('home', compact('res', 'name'));
+});
+
+
+Route::get('/about', function () {
+    return '<h1>About page</h1>';
 });
