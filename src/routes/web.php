8486ae02698e03854dacc37ddf1c129f6ad23e98
vladismeno <vladismeno@gmail.com>
2024-11-22 22:55:59 -0800

    Lesson 5

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,3 +12,38 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return '<h1>About page</h1>';
 });
+
+/*Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::post('/send-email', function () {
+    if (!empty($_POST)) {
+        dump($_POST);
+    }
+    return 'Send Email';
+});*/
+
+/*Route::match(['get', 'post'], '/contact', function () {
+    if (!empty($_POST)) {
+        dump($_POST);
+    }
+
+    return view('contact');
+});*/
+
+/*Route::match(['get', 'post'], '/contact', function () {
+    if (!empty($_POST)) {
+        dump($_POST);
+    }
+
+    return view('contact');
+})->name('contact');*/
+
+//Route::view('/test', 'test', ['test' => 'Test Data']);
+
+//Route::redirect('about', 'contact');
+//Route::redirect('about', 'contact', 301);
+
+//Route::permanentRedirect('about', 'contact');
+
